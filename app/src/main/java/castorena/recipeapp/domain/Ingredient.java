9f32af9e227b03b6ffee1ab17ace2769d53acb74
Omar Castorena <omarcastorena11@gmail.com>
2020-11-05 20:53:29 -0700

    added cache implementation

--- app/src/main/java/castorena/recipeapp/domain/Ingredient.java
+++ app/src/main/java/castorena/recipeapp/domain/Ingredient.java
@@ -1,6 +1,7 @@
 package castorena.recipeapp.domain;
 
 public class Ingredient {
+    private int id;
     private String name;
     private String category;
 
@@ -18,5 +19,13 @@ public class Ingredient {
 
     public void setCategory(String category) {
         this.category = category;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
     }
 }
--- app/src/main/java/castorena/recipeapp/service/IngredientSvcCacheImpl.java
+++ app/src/main/java/castorena/recipeapp/service/IngredientSvcCacheImpl.java
@@ -7,10 +7,56 @@ import castorena.recipeapp.domain.Ingredient;
 
 public class IngredientSvcCacheImpl implements IngredientSvcInt{
 
-    private List<Ingredient> ingredients = new LinkedList<Ingredient>();
+    private int nextId = 0;
+    private List<Ingredient> ingredients = new LinkedList<>();
+
+    private IngredientSvcCacheImpl() {
+        initList();
+    }
+
+    private void initList() {
+        Ingredient ingredient = new Ingredient();
+        ingredient.setName("Carrot");
+        ingredient.setCategory("Vegetables");
+        ingredients.add(ingredient);
+    }
+
+    private static IngredientSvcCacheImpl instance = new IngredientSvcCacheImpl();
+
+    public static IngredientSvcCacheImpl getInstance() {
+        return instance;
+    }
 
     public Ingredient create(Ingredient ingredient) {
+        ingredient.setId(++nextId);
         ingredients.add(ingredient);
+        return ingredient;
+    }
+
+    public List<Ingredient> retrieveAll() {
+        return ingredients;
+    }
+
+    public Ingredient update(Ingredient ingredient) {
+        int size = ingredients.size();
+        for (int i = 0; i < size; i++) {
+            if (ingredients.get(i).getId() == ingredient.getId()) {
+                ingredients.set(i, ingredient);
+                break;
+            }
+        }
+        return ingredient;
+    }
+
+    public Ingredient delete(Ingredient ingredient) {
+        int size = ingredients.size();
+        for (int i = 0; i < size; i++) {
+            if (ingredients.get(i).getId() == ingredient.getId()) {
+                ingredients.remove(i);
+                break;
+            }
+        }
+        return ingredient;
     }
 
 }
